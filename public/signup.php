e535c3f5f6957d153769b093f9a1faf2e7014404
ikaliiev <illyakk1989@gmail.com>
2022-06-20 11:39:03 +0300

    beautify

--- public/signup.php
+++ public/signup.php
@@ -17,7 +17,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 }
-$sql = "INSERT INTO users (firstname, email, phone, password) VALUES ('".$name."','".$email."', '".$phone."', '".$pass."')"
+$sql = "INSERT INTO main (firstname, email, phone, password) VALUES ('".$name."','".$email."', '".$phone."', '".$pass."')"
 if ($conn->query($sql) === TRUE) {
 	echo 1;
 } else {

3692927f2b63091961a4218734fb1d4ecf164431
ikaliiev <illyakk1989@gmail.com>
2022-06-20 11:07:17 +0300

    registation form

---
+++ public/script.js
@@ -0,0 +1,62 @@
+document.querySelector('#signup-submit').onclick = function(event){
+	event.preventDefault();
+	console.log('work'); 
+	let name = document.querySelector('#signup-name');
+	let email = document.querySelector('#signup-email');
+	let phone = document.querySelector('#signup-phone');
+	let pass = document.querySelector('#signup-pass');
+	let data = {
+		"name" :name;
+		"email":email;
+		"phone":phone;
+		"pass":pass;
+	}
+
+	ajax('signup.php','POST',signup,dataArray);
+
+	function signup(result){
+       console.log(result) ;
+       if (result ==2) {
+       	   alert('Заполните поля');
+       	   else if (result ==1) {
+       	   	     alert('Успех. Теперь можно войти!')
+
+       	   }
+       	   else {
+       	   	     alert('Ошибка, повторите регистрацию позже!');
+       	   }
+       }
+
+       
+
+	}
+}
+
+document.querySelector('#login-submit').onclick = function(event){
+	event.preventDefault();
+	let email = document.querySelector('#login-email');
+	let pass = document.querySelector('#login-pass');
+	let data = {
+		"email":email;
+		"pass":pass;
+	}
+
+	ajax('core/login.php','POST',login,dataArray);
+
+	function login(result){
+       console.log(result) ;
+       if (result ==2) {
+       	   alert('Заполните поля');
+       	   else if (result ==0) {
+       	   	     alert('Такой пользователь не найден!')
+
+       	   }
+       	   else {
+       	   	     console.log(result);
+       	   }
+       }
+
+       
+
+	}
+}
---
+++ public/signup.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'config.php';
+
+$name = trim($_POST['firstname']);
+$email = trim($_POST['email']);
+$phone = trim($_POST['phone']);
+$pass = trim($POST['password']);
+
+if ($name == '' OR $email == '' OR $phone=='' OR $pass ==''){
+   echo 2;
+   die;
+}
+
+//Create connectiom
+$conn = new mysqli($servername, $username, $password, $dbname);
+//Check connection
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO users (firstname, email, phone, password) VALUES ('".$name."','".$email."', '".$phone."', '".$pass."')"
+if ($conn->query($sql) === TRUE) {
+	echo 1;
+} else {
+	echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
